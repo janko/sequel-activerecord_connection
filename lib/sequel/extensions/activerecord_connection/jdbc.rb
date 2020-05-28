2e48ac4a92dbb3b0ec3e1564a248842df789d38f
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-05-28 13:43:10 +0100

    added necessary wrappers for postgresql

--- lib/sequel/extensions/activerecord_connection/jdbc.rb
+++ lib/sequel/extensions/activerecord_connection/jdbc.rb
@@ -2,12 +2,30 @@ module Sequel
   module ActiveRecordConnection
     module Jdbc
       def statement(conn)
-        stmt = conn.connection.createStatement
+        stmt = activerecord_raw_connection.connection.createStatement
         yield stmt
-      rescue *DATABASE_ERROR_CLASSES => e
-        raise_error(e)
+      rescue ActiveRecord::StatementInvalid => exception
+        raise_error(exception.cause, classes: database_error_classes)
+      rescue *database_error_classes => e
+        raise_error(e, classes: database_error_classes)
       ensure
         stmt.close if stmt
+      end
+
+      def execute(sql, opts=OPTS)
+        activerecord_connection.send(:log, sql) do
+          super
+        end
+      rescue ActiveRecord::StatementInvalid => exception
+        raise_error(exception.cause, classes: database_error_classes)
+      end
+
+      def execute_dui(sql, opts=OPTS)
+        activerecord_connection.send(:log, sql) do
+          super
+        end
+      rescue ActiveRecord::StatementInvalid => exception
+        raise_error(exception.cause, classes: database_error_classes)
       end
     end
   end
