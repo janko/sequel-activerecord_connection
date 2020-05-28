8da426cf4a725f35b0f6bff8b63450f397379fd3
HoneyryderChuck <cardoso_tiago@hotmail.com>
2020-05-28 15:13:36 +0100

    fixing transaction log for sqlite

--- test/sqlite_test.rb
+++ test/sqlite_test.rb
@@ -31,9 +31,9 @@ describe "sqlite3 connection" do
     assert_equal [:id, :col, :time], @db[:records].columns
 
     assert_logged <<-SQL.strip_heredoc
-      begin transaction
+      #{RUBY_ENGINE == "jruby" ? "BEGIN TRANSACTION" : "begin transaction"}
       INSERT INTO `records` (`col`) VALUES ('a'), ('b'), ('c')
-      commit transaction
+      #{RUBY_ENGINE == "jruby" ? "COMMIT TRANSACTION" : "commit transaction"}
       SELECT * FROM `records` ORDER BY `id`
     SQL
   end
